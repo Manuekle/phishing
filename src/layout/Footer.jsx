c60722a8c72343b6bb231bd859eb05a937831ea2
Manuel Erazo <elustondo129@gmail.com>
2022-09-21 20:17:08 -0500

    footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <section className="lg:mt-56 mt-28 relative w-full bg-gradient-to-r from-shad-yellow-100 via-shad-yellow-100 to-shad-orange-100">
       <div className="flex justify-center">
-        <div className="flex flex-col lg:pt-20 lg:pb-20 pb-10 pt-10">
+        <div className="flex flex-col lg:pt-20 lg:pb-20 pb-20 pt-10">
           <h1 className="flex-col lg:flex-row items-center gap-2 flex font-bold text-zinc-800 text-xl lg:text-2xl text-center uppercase tracking-wider">
             Desarrollado por{' '}
             <a
